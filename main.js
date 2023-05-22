5ea708d988b6a3db604c0ecd2613c607ec9facc1
Xaperoll <96544309+Xaperoll@users.noreply.github.com>
2023-05-22 22:47:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function goTo(name) {
+	document.querySelector(name).scrollIntoView({block: "center", behavior: "smooth"});
+}
+
+function setPlayerMax(e) {
+	document.querySelector('div.number-players').innerText = 'Number of players ' + e.value;
+}
+
+function setCode() {
+	let codes = document.querySelectorAll('div.select-code > input[type="range"]');
+	var code = '';
+	for (var i = codes.length - 1; i >= 0; i--) {
+		code = codes[i].value + code;
+	}
+	document.querySelector('div.room-code').innerText = 'Entered code '+ code;
+}
